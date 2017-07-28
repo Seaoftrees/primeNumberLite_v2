33826a83fb51afa275399ebdcb146aad9b2afe2a
Seaoftrees <Seaoftrees@users.noreply.github.com>
2017-07-28 12:18:38 +0900

    Add files via upload

---
+++ primeNumberLite/primeNumberLite/Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿namespace primeNumberLite
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(145, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(327, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "素数判別・リスト化ソフト";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.numericUpDown1.Location = new System.Drawing.Point(87, 84);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            -989331456,
+            -104015711,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(393, 23);
+            this.numericUpDown1.TabIndex = 1;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "1.値を入力";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "2.モードを選択";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(92, 126);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 16);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "素数判別";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(92, 148);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(59, 16);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.Text = "リスト化";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label4.Location = new System.Drawing.Point(172, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(198, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "入力した値が素数かどうかを判別します。";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label5.Location = new System.Drawing.Point(172, 152);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(193, 12);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "入力した値までの素数をリスト化します。";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 198);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 12);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "3.計算する";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(87, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "計算開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label7.Location = new System.Drawing.Point(486, 95);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(147, 12);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "2~1800京まで入力可能です。";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 270);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(37, 12);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "4.結果";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label9.Location = new System.Drawing.Point(63, 307);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(271, 24);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "結果がここに表示されます。";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(427, 121);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(199, 197);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label10.Location = new System.Drawing.Point(65, 270);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(223, 12);
+            this.label10.TabIndex = 6;
+            this.label10.Text = "割りきれた数字またはリストが右に表示れます。";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(427, 326);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(199, 23);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "テキストボックスの中身をコピーする";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(87, 222);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(334, 23);
+            this.progressBar1.TabIndex = 14;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(87, 170);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(111, 16);
+            this.checkBox1.TabIndex = 15;
+            this.checkBox1.Text = "マルチタスクで実行";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Enabled = false;
+            this.numericUpDown2.Location = new System.Drawing.Point(214, 169);
+            this.numericUpDown2.Maximum = new decimal(new int[] {
+            524288,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(59, 19);
+            this.numericUpDown2.TabIndex = 16;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label11.Location = new System.Drawing.Point(279, 176);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(130, 12);
+            this.label11.TabIndex = 6;
+            this.label11.Text = "スレッド数を指定できます。";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("MS UI Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label12.ForeColor = System.Drawing.Color.Navy;
+            this.label12.Location = new System.Drawing.Point(248, 191);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(167, 11);
+            this.label12.TabIndex = 6;
+            this.label12.Text = "論理プロセッサ数*1024 が最大です。";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 361);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "primeNumber Lite v2";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+    }
+}
+
---
+++ primeNumberLite/primeNumberLite/Form1.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace primeNumberLite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //フォームが読み込まれた処理
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //スパコン向けに一応...ね?(エラーで動かないけどねｗ)
+            int core = Environment.ProcessorCount;
+            if (core < 16384)//816384=2^23 (*1024するとint超える)
+            {
+                numericUpDown2.Maximum = core * 1024;
+            }
+            else
+            {
+                numericUpDown2.Maximum = 2147483647;//int数の最大
+            }
+        }
+
+        //テキストボックスの中身をコピー
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox1.Text);
+        }
+
+        //リスト化モードになったとき
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Enabled = true;
+            if (checkBox1.Checked)
+            {
+                numericUpDown2.Enabled = true;
+            }
+        }
+
+        //判別モードになったとき
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Enabled = false;
+            numericUpDown2.Enabled = false;
+        }
+
+        //マルチタスク化ボタンのチェックが変わったとき
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            numericUpDown2.Enabled = !numericUpDown2.Enabled;
+        }
+
+        //計算処理(メインプロセス)
+        public void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            textBox1.Text = null;
+            progressBar1.Value = 0;
+            try
+            {
+                sw.Start();
+                ulong n = (ulong)numericUpDown1.Value;
+
+                //素数判別モード
+                if (radioButton1.Checked)
+                {
+                    if (n == 2)
+                    {
+                        sw.Stop();
+                        label9.Text = "素数です。(" + (int)sw.Elapsed.TotalMilliseconds + "ms)";
+                        progressBar1.Value = 100;
+                    }
+                    else if (n % 2 == 0)
+                    {
+                        sw.Stop();
+                        label9.Text = "素数ではありません。(" + (int)sw.Elapsed.TotalMilliseconds + "ms)";
+                        textBox1.Text = "2";
+                        progressBar1.Value = 100;
+                    }
+                    else
+                    {
+                        double sqrt = Math.Sqrt(n);
+                        long result = 0;
+                        int forProgress = (int)(sqrt - 3) / 100;
+                        for (long i = 3; i <= sqrt; i += 2)
+                        {
+                            if (n % (ulong)i == 0)
+                            {
+                                result = i;
+                                break;
+                            }
+
+                            if (i > forProgress * (progressBar1.Value + 1) && progressBar1.Value < 100)
+                            {
+                                progressBar1.Value += 1;
+                            }
+                        }
+
+                        if (result == 0)
+                        {
+                            sw.Stop();
+                            label9.Text = "素数です。(" + (int)sw.Elapsed.TotalMilliseconds + "ms)";
+                            progressBar1.Value = 100;
+                        }
+                        else
+                        {
+                            sw.Stop();
+                            label9.Text = "素数ではありません。(" + (int)sw.Elapsed.TotalMilliseconds + "ms)";
+                            textBox1.Text = $"{result}";
+                            progressBar1.Value = 100;
+                        }
+                    }
+
+                }//リストモード
+                else
+                {
+
+                    //マルチスレッドを選択してないとき(旧プログラム)
+                    if (!checkBox1.Checked)
+                    {
+                        textBox1.Text = "2\r\n";
+                        int forProgress = (int)(n - 3) / 100;
+                        int[] primeLiteList = new int[21];
+                        for (ulong k = 3; k <= n; k += 2)
+                        {
+
+                            double sqrt = Math.Sqrt(k);
+                            long result = 0;
+                            for (long i = 3; i <= sqrt; i += 2)
+                            {
+                                if (k % (ulong)i == 0)
+                                {
+                                    result = i;
+                                    break;
+                                }
+                            }
+
+                            if (result == 0)
+                            {
+                                textBox1.Text = textBox1.Text + k + "\r\n";
+                            }
+
+
+                            if (k > (ulong)(forProgress * (progressBar1.Value + 1)) && progressBar1.Value < 100)
+                            {
+                                progressBar1.Value += 1;
+                            }
+                        }
+                        sw.Stop();
+                        int time = (int)sw.Elapsed.TotalMilliseconds;
+                        if (sw.Elapsed.TotalHours > 1)
+                        {
+                            label9.Text = "リスト化が終了しました。(" + sw.Elapsed.Hours + "h"
+                                + sw.Elapsed.Minutes + "m"
+                                + sw.Elapsed.Seconds + "s)";
+                        }
+                        else if (time > 60000)
+                        {
+                            label9.Text = "リスト化が終了しました。(" + sw.Elapsed.Minutes + "m" +
+                                sw.Elapsed.Seconds + "s)";
+                        }
+                        else if (time > 10000)
+                        {
+                            label9.Text = "リスト化が終了しました。(" + (int)sw.Elapsed.TotalSeconds + "s)";
+                        }
+                        else
+                        {
+                            label9.Text = "リスト化が終了しました。(" + (int)sw.Elapsed.TotalMilliseconds + "ms)";
+                        }
+                        progressBar1.Value = 100;
+
+
+                    }
+                    else
+                    {
+                        //マルチスレッド有効化時
+                        int Threads = (int)numericUpDown2.Value;
+                        textBox1.Text = "2\r\n";
+                        int forProgress = (int)(n - 3) / 100;
+
+                        if (n > 2)
+                        {
+                            int boost = Threads / 1024;
+                            ulong p = 3;
+                            bool end = false;//一時停止ボタン用?
+                            while (!end)
+                            {
+                                var tr = ParentTask(p, Threads, n);
+                                tr.Start();
+                                textBox1.Text = textBox1.Text + tr.Result;
+
+                                //p += (ulong)(Threads*boost*2);
+                                p += (ulong)Threads * 2;
+
+                                if (p > (ulong)(forProgress * (progressBar1.Value + 1)) && progressBar1.Value < 100)
+                                {
+                                    progressBar1.Value += 1;
+                                }
+
+                                if (p > n)
+                                {
+                                    end = true;
+                                    break;
+                                }
+                            }
+                        }
+
+                        sw.Stop();
+                        int time = (int)sw.Elapsed.TotalMilliseconds;
+                        if (sw.Elapsed.TotalHours > 1)
+                        {
+                            label9.Text = "リスト化が終了しました。(" + sw.Elapsed.Hours + "h"
+                                + sw.Elapsed.Minutes + "m"
+                                + sw.Elapsed.Seconds + "s)";
+                        }
+                        else if (time > 60000)
+                        {
+                            label9.Text = "リスト化が終了しました。(" + sw.Elapsed.Minutes + "m" +
+                                sw.Elapsed.Seconds + "s)";
+                        }
+                        else if (time > 10000)
+                        {
+                            label9.Text = "リスト化が終了しました。(" + (int)sw.Elapsed.TotalSeconds + "s)";
+                        }
+                        else
+                        {
+                            label9.Text = "リスト化が終了しました。(" + (int)sw.Elapsed.TotalMilliseconds + "ms)";
+                        }
+                        progressBar1.Value = 100;
+
+
+                    }
+
+                }
+
+            }
+            catch(Exception ex)
+            {
+                label9.Text = "エラー(ErrorMessageは右)";
+                textBox1.Text = ex.Message;
+            }
+        }
+
+        //さらなる親タスク(実験段階につき未実装)
+        public String GrandParentTask(ulong p , int Threads,int boost, ulong n)
+        {
+            String result = "";
+            Task[] tasks = new Task[boost];
+            for (int i=0; i<boost; i++)
+            {
+                var task = ParentTask(p, Threads, n);
+                task.Start();
+                tasks[i] = task;
+                result = result + task.Result;
+                p += (ulong)Threads * 2;
+            }
+            Task.WaitAll(tasks);
+            return result;
+        }
+
+        //loop
+        public Task<String> ParentTask(ulong p, int Threads, ulong n)
+        {
+            String result = "";
+            Task[] tasks = new Task[Threads];//8byte * Threads 1Gとすると限界は64threadが限界?(ではないみたい)
+            List<ulong> results = new List<ulong>();
+
+            for (int t = 0; t < Threads; t++)
+            {
+                //tasks[t] = Task<ulong>.Factory.StartNew(() => PrimeTask(p));
+                var task = new Task<ulong>(() => PrimeTask(p));
+                task.Start();
+                tasks[t] = task;
+                results.Add(task.Result);
+                p += 2;
+            }
+            Task.WaitAll(tasks);
+
+            results.Sort();
+            foreach (ulong q in results)
+            {
+                if (q != 0)
+                {
+                    if (q > n)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        result = result + q + "\r\n";
+                    }
+                }
+            }
+
+            var LastResult = new Task<String>(() =>{ return result; });
+            return LastResult;
+
+        }
+
+        //Task (素数じゃないなら0が返る)
+        public ulong PrimeTask(ulong p)
+        {
+            ulong result = 0;
+            if (WetherPrime(p))
+            {
+                result = p;
+            }
+            return result;
+        } 
+
+        //素数判別処理 (素数ならtrue)
+        public bool WetherPrime(ulong p)
+        {
+            bool notPrime = false;
+
+            if (p % 2 == 0)
+            {
+                if (p != 2)
+                {
+                    notPrime = true;
+                }
+            }
+            else if(p != 3)
+            {
+                double sqrt = Math.Sqrt(p);
+                for (long i = 3; i <= sqrt; i += 2)
+                {
+                    if (p % (ulong)i == 0)
+                    {
+                        notPrime = true;
+                        break;
+                    }
+                }
+            }
+
+            return !notPrime;
+        }
+
+    }
+}
